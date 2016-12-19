8c502b99ace4d12c3f9cb61c630ac3fdf07d14ee
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-19 16:51:41 +0530

    put digits when number buttons are clicked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+function numberInput(){
+	console.log("Running Number Input")
+	$(".num").click(function(){
+		console.log("num clicked");
+		var digit = $(this).attr("value");
+		console.log(digit);
+		$("#digits-entry").val($("#digits-entry").val() + digit);
+	});
+}
+
+function calculate() {
+	console.log("Running Calculate");
+	numberInput();
+}
 
 
+$(calculate());
