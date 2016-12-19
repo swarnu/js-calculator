870369b8729763918c62db4d0aac7cf0c1e60f33
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-19 17:02:26 +0530

    if period is pressed first then prepend 0

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ function numberInput(){
 	console.log("Running Number Input")
 	$(".num").click(function(){
 		console.log("num clicked");
+		
 		var digit = $(this).attr("value");
 		console.log(digit);
-		$("#digits-entry").val($("#digits-entry").val() + digit);
+		
+		var digitEntry = $("#digits-entry").val();
+		console.log(digitEntry);
+		
+		if (digit == "." && digitEntry == "") {
+			$("#digits-entry").val( "0" + digit);
+		}
+		else  {
+			$("#digits-entry").val(digitEntry + digit);
+		}
 	});
 }
 
